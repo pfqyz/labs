9a3089e596c8feae2bc545ee1856739072fc0915
local user <apelsinsofa@gmail.com>
2024-10-05 14:53:21 +0300

    Add is leap year programm

---
+++ DateTime.pas
@@ -0,0 +1,8 @@
+﻿begin
+  var year :=ReadInteger('Введите год');
+  Assert(year>0);
+  if (year.Divs(4)) and (not(year.Divs(100)) or (year.Divs(400))) then
+    Print(True)
+  else
+    Print(False);
+end.
