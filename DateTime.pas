eb76280ce9119a75796a5c8d4b7d4d3660740f8b
local user <apelsinsofa@gmail.com>
2024-10-05 15:23:22 +0300

    Add what day is closer to New year

--- DateTime.pas
+++ DateTime.pas
@@ -5,4 +5,18 @@
     Print(True)
   else
     Print(False);
+  
+  var (q,w):=RedInteger2('Введите 2 числа( день и месяц числа):');
+  var (a,b):=RedInteger2('Введите 2 числа( день и месяц числа):');
+  if w>b then
+    Print($'{q}.{w}');
+  else if b>w
+    Print($'{a}.{b}');
+  else if q>a
+    Print($'{q}.{w}');
+  else if a>q
+    Print($'{a}.{b}')
+  else
+    Print('Даты одинаковые');
+
 end.
